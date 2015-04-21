f345c75be1f7a858baf70a2bb684d01e5111d399
LipingSun <liping.sun@sjsu.edu>
2015-04-21 01:59:29 -0700

    achieve get emulators and get emulator details

--- routes/emulator.js
+++ routes/emulator.js
@@ -1,47 +1,84 @@
+var http = require('http');
 var request = require('request');
 express = require('express');
 
-//var emulatorIP = '8.21.28.197';
+var url = 'http://8.21.28.162:8888/emulator';
 
 var emulator = express();
 
 emulator.get('/', function (req, res) {
-    var emulatorIP = '8.21.28.197';
-    request.get(emulatorIP + ':8888/emulator', function(emulatorRes) {
-        console.log("Got response: " + emulatorRes);
-        res.end(emulatorRes);
-    }).on('error', function(e) {
-        console.log("Got error: " + e.message);
+    request.get(url, function(error, response, body) {
+        if (!error) {
+            console.log('Response Code: ' + response.statusCode);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(body.toString());
+        } else {
+            console.log('Error: ' + error);
+        }
     });
 });
 
 emulator.get('/:id', function (req, res) {
-    var emulatorIP = '8.21.28.197';
-    request.get(emulatorIP + ':8888/emulator?id=' + req.params.id, function(emulatorRes) {
-        console.log("Got response: " + emulatorRes);
-        res.end(emulatorRes);
-    }).on('error', function(e) {
-        console.log("Got error: " + e.message);
+    request.get(url + '?id=' + req.params.id, function(error, response, body) {
+        if (!error) {
+            console.log('Response code: ' + response.statusCode);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(body.toString());
+        } else {
+            console.log('Error: ' + error);
+        }
     });
 });
 
 emulator.post('/', function (req, res) {
-    var emulatorIP = '8.21.28.197';
-    request.post(emulatorIP + ':8888/emulator', req.body, function(emulatorRes) {
-        console.log("Got response: " + emulatorRes);
-        res.end(emulatorRes);
-    }).on('error', function(e) {
+    request.post(url, JSON.stringify({'id': 3333}), function(error, response, body) {
+        if (!error) {
+            console.log('Response code: ' + response.statusCode);
+            console.log('Response: ' + body.toString());
+            res.setHeader('Content-Type', 'application/json');
+            res.end(body.toString());
+        } else {
+            console.log('Error: ' + error);
+        }
+    });
+
+ /*   var opts = {
+        host: '8.21.28.162',
+        port: 8888,
+        method: 'POST',
+        path: '/emulator',
+        headers: {}
+    };
+    opts.headers['Content-Type'] = 'application/json';
+    req.data = JSON.stringify(req.body);
+    opts.headers['Content-Length'] = req.data.length;
+    var req = http.request(opts, function(response) {
+        response.on('data', function(chunk) {
+            res_data += chunk;
+        });
+        response.on('end', function() {
+            callback(res_data);
+        });
+    });
+    req.on('error', function(e) {
         console.log("Got error: " + e.message);
     });
+// write the data
+    if (opts.method != 'GET') {
+        req.write(req.data);
+    }
+    req.end();*/
 });
 
-emulator.delete('/:id', function (req, res) {
-    var emulatorIP = '8.21.28.197';
-    request.get(emulatorIP + ':8888/emulator?id=' + req.params.id, function(emulatorRes) {
-        console.log("Got response: " + emulatorRes);
-        res.end(emulatorRes);
-    }).on('error', function(e) {
-        console.log("Got error: " + e.message);
+emulator.del('/:id', function (req, res) {
+    request.del(url, {'id' : req.params.id}, function(error, response, body) {
+        if (!error) {
+            console.log('Response code: ' + response.statusCode);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(body.toString());
+        } else {
+            console.log('Error: ' + error);
+        }
     });
 });
 
